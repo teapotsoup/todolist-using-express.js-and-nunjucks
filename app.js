2012c91b6ab6930f82542b8b3c6c20cdb2082413
Seo Jeong Woo <64897060+teapotsoup@users.noreply.github.com>
2022-02-13 00:19:50 +0900

    first
    node_modules는 없으니 npm install로 다운해주세요

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express'), path = require('path'), nunjucks = require('nunjucks');
+const app = express();
+app.set('port', process.env.PORT || 3000);
+
+const toDoLists = ["밥먹기"]
+
+app.set('view engine' ,'html');
+
+nunjucks.configure('views', {
+  express: app,
+  watch: true,
+});
+
+// app.use(express.static(__dirname + '/public'));
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+app.get('/', (req, res) => {
+  res.render('index', {toDoListTitle: '할 일 갯수 : ' + toDoLists.length, toDoLists:toDoLists});
+});
+
+app.post('/add_list', (req, res)=>{
+  const newContent = req.body.content;
+  console.log(newContent+' 추가');
+  toDoLists.push(newContent);
+  res.redirect('/');
+})
+
+app.listen(app.get('port'), () => {
+  console.log(app.get('port'), '번 포트에서 대기 중');
+});
---
+++ todo.js
@@ -0,0 +1,3 @@
+const crudForm = document.querySelector("#crudForm");
+const crudInput = document.querySelector("#crudForm Input");
+const crudList = document.querySelector("#crudList");
